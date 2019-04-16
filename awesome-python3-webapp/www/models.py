0f983a90fe9d8b14aa3f665180170d0c4e2c0be2
unknown <347820090@qq.com>
2019-04-16 23:51:48 +0800

    3

---
+++ awesome-python3-webapp/www/models.py
@@ -0,0 +1,68 @@
+import time, uuid
+#UUID是128位的全局唯一标识符，通常由32字节的字符串表示。它可以保证时间和空间的唯一性，也称为GUID，全称为：UUID —— Universally Unique IDentifier Python 中叫 UUID，GUID —— Globally Unique IDentifier C# 中叫 GUID它通过MAC地址、时间戳、命名空间、随机数、伪随机数来保证生成ID的唯一性
+
+from orm import Model, StringField, BooleanField, FloatField, TextField
+'''1、uuid1()——基于时间戳
+        
+               由MAC地址、当前时间戳、随机数生成。可以保证全球范围内的唯一性，
+               但MAC的使用同时带来安全性问题，局域网中可以使用IP来代替MAC。
+
+       2、uuid2()——基于分布式计算环境DCE（Python中没有这个函数）
+
+                算法与uuid1相同，不同的是把时间戳的前4位置换为POSIX的UID。
+                实际中很少用到该方法。
+
+      3、uuid3()——基于名字的MD5散列值
+             
+                通过计算名字和命名空间的MD5散列值得到，保证了同一命名空间中不同名字的唯一性，
+                和不同命名空间的唯一性，但同一命名空间的同一名字生成相同的uuid。
+ 
+       4、uuid4()——基于随机数
+
+                由伪随机数得到，有一定的重复概率，该概率可以计算出来。
+
+       5、uuid5()——基于名字的SHA-1散列值
+
+                算法与uuid3相同，不同的是使用 Secure Hash Algorithm 1 算法
+
+'''
+def next_id():
+    return '%015d%s000' % (int(time.time() * 1000), uuid.uuid4().hex)
+
+#下面是三个表的数据类
+#创建一个User类，就是表里的数据
+#default可以自动生成缺省值，保证每次插入数据id不会重复
+class User(Model):
+#mysql中表的名字，这里的table是model的属性，详见orm源码
+    __table__ = 'users'
+
+    id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
+    email = StringField(ddl='varchar(50)')
+    passwd = StringField(ddl='varchar(50)')
+    admin = BooleanField()
+    name = StringField(ddl='varchar(50)')
+    image = StringField(ddl='varchar(500)')
+    created_at = FloatField(default=time.time)
+
+class Blog(Model):
+    __table__ = 'blogs'
+
+    id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
+    user_id = StringField(ddl='varchar(50)')
+    user_name = StringField(ddl='varchar(50)')
+    user_image = StringField(ddl='varchar(500)')
+    name = StringField(ddl='varchar(50)')
+    summary = StringField(ddl='varchar(200)')
+    content = TextField()
+    created_at = FloatField(default=time.time)
+
+class Comment(Model):
+    __table__ = 'comments'
+
+    id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
+    blog_id = StringField(ddl='varchar(50)')
+    user_id = StringField(ddl='varchar(50)')
+    user_name = StringField(ddl='varchar(50)')
+    user_image = StringField(ddl='varchar(500)')
+    content = TextField()
+    created_at = FloatField(default=time.time)
